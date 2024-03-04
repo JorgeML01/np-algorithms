6699413a52c1544caff6efc771b1d64c71374a1a
Jorge Matute <jorgedd2001@gmail.com>
2024-03-04 12:14:51 -0600

    tsp refresh node number

--- src/Components/TravelingSalespersonBox/TravelingSalespersonBox.jsx
+++ src/Components/TravelingSalespersonBox/TravelingSalespersonBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sketch from "react-p5";
 
 function factorialRecursivo (n) {
@@ -29,10 +29,12 @@ function dist(x0, y0, x1, y1) {
 
 export default function TravelingSalespersonBox(props) {
 
+    const [totalCities, setTotalCities] = useState(props.totalCities);
+
     let cities = [];
     let totalTimes = [];
     let totalDistances = [];
-    let totalCities = 6;
+    //let totalCities = props.totalCities;
 
     let recordDistance;
     let bestEver;
@@ -45,8 +47,28 @@ export default function TravelingSalespersonBox(props) {
     let endTime = 0;
 
   
+    useEffect(() => {
+        // Este efecto se ejecutará cuando totalCities cambie
+        setTotalCities(props.totalCities);
+    }, [props.totalCities]);
+
+
+    function resetVariables(p5) {
+        setTotalCities(props.totalCities);
+        cities = [];
+        totalTimes = [];
+        totalDistances = [];
+        recordDistance = undefined;
+        bestEver = undefined;
+        contador = 0;
+        max = factorialRecursivo(totalCities);
+        endTime = 0;
+        console.log(props.totalCities);
+    }
+
+
     function setup(p5, canvasParentRef) {
-        p5.createCanvas(1200, 950).parent(canvasParentRef);
+        p5.createCanvas(400, 400).parent(canvasParentRef);
 
         for (let i = 0; i < totalCities; i++) {
             let v = p5.createVector(p5.random(p5.width), p5.random(p5.height));
@@ -61,60 +83,66 @@ export default function TravelingSalespersonBox(props) {
     function draw(p5) {
         p5.background(0);
         p5.fill(255);
-
-        for (let i = 0; i < cities.length; i++) {
-            p5.ellipse(cities[i].x, cities[i].y, 8, 8);
+    
+        // Verifica si cities está definido antes de usarlo
+        if (cities && cities.length > 0) {
+            for (let i = 0; i < cities.length; i++) {
+                p5.ellipse(cities[i].x, cities[i].y, 8, 8);
+            }
+    
+            p5.stroke(255);
+            p5.strokeWeight(1);
+            p5.noFill();
+    
+            p5.beginShape();
+            for (let i = 0; i < cities.length; i++) {
+                p5.vertex(cities[i].x, cities[i].y);
+            }
+            p5.endShape();
+    
+            p5.stroke(255, 0, 255);
+            p5.strokeWeight(4);
+            p5.noFill();
+            p5.beginShape();
+            contador++;
+            for (let i = 0; i < cities.length; i++) {
+                p5.vertex(bestEver[i].x, bestEver[i].y);
+            }
+            if (contador < max) {
+                console.log(contador);
+            } else if (contador === max) {
+                console.log("Mejor distancia: " + recordDistance);
+                console.log("Tiempo tomado para la mejor ruta: " + endTime / 1000);
+                console.log("Total distances: " + totalDistances);
+                console.log("Total times: " + totalTimes);
+            }
+            p5.endShape();
+    
+            var i = p5.floor(p5.random(cities.length));
+            var j = p5.floor(p5.random(cities.length));
+    
+            if (contador < max) {
+                swap(cities, i, j);
+            }
+    
+            var d = calcDistance(cities);
+            if (d < recordDistance) {
+                recordDistance = d;
+                bestEver = cities.slice();
+                console.log(recordDistance);
+                endTime = performance.now();
+                totalTimes.push(endTime / 1000);
+                totalDistances.push(recordDistance);
+            }
         }
-
-        p5.stroke(255);
-        p5.strokeWeight(1);
-        p5.noFill();
-
-
-        p5.beginShape();
-        for (let i = 0; i < cities.length; i++) {
-            p5.vertex(cities[i].x, cities[i].y);
-        }
-        p5.endShape();
-     
-
-        p5.stroke(255, 0, 255);
-        p5.strokeWeight(4);
-        p5.noFill();
-        p5.beginShape();
-        contador++;
-        for (let i = 0; i < cities.length; i++) {
-            p5.vertex(bestEver[i].x, bestEver[i].y);
-        }
-        if (contador < max) {
-            console.log(contador);
-        } else if (contador == max) {
-            console.log("Mejor distancia: " + recordDistance);
-            console.log("Tiempo tomado para la mejor ruta: " + endTime/1000);
-            console.log("Total distances: " + totalDistances);
-            console.log("Total times: " + totalTimes);
-        }
-        p5.endShape();
-
-        var i = p5.floor(p5.random(cities.length));
-        var j = p5.floor(p5.random(cities.length));
-
-        if (contador < max){
-            swap(cities, i, j);
-        }
-
-        var d = calcDistance(cities);
-        if (d < recordDistance) {
-          recordDistance = d;
-          bestEver = cities.slice();
-          console.log(recordDistance);
-          endTime = performance.now();
-          totalTimes.push(endTime/1000);
-          totalDistances.push(recordDistance);
-        }
-
     }
+    
 
-  return <Sketch setup={setup} draw={draw} />;
+    return (
+        <div>
+            <Sketch setup={setup} draw={draw} />
+            <button onClick={() => resetVariables()}>Reiniciar</button>
+        </div>
+    );
 }
 
--- src/Pages/TravelingSalesperson/TravelingSalesperson.jsx
+++ src/Pages/TravelingSalesperson/TravelingSalesperson.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TravelingSalespersonBox from "../../Components/TravelingSalespersonBox/TravelingSalespersonBox";
 
 function TravelingSalesperson() {
+    const [totalCities, setTotalCities] = useState(7);
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    const handleTotalCitiesChange = (newTotalCities) => {
+        setTotalCities(newTotalCities);
+    };
+
+    const handleRefresh = () => {
+        // Incrementar el valor de refreshKey
+        setRefreshKey((prevKey) => prevKey + 1);
+    };
+
     return (
         <div>
-            <TravelingSalespersonBox />
+            <input
+                type="number"
+                value={totalCities}
+                onChange={(e) => handleTotalCitiesChange(e.target.value)}
+            />
+            <button onClick={handleRefresh}>Incrementar Key</button>
+            <TravelingSalespersonBox key={refreshKey} totalCities={totalCities} />
         </div>
     );
 }

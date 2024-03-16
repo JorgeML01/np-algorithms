929813db2057cfb3fcb8a73d193817f9e63bf1bd
Dessire1012 <dessire@unitec.edu>
2024-03-15 20:57:57 -0600

    Pequeños cambios visuales

--- src/Pages/ColoracionGrafos/ColoracionGrafos.jsx
+++ src/Pages/ColoracionGrafos/ColoracionGrafos.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Sketch from "react-p5";
+import Button from "react-bootstrap/Button";
 
 const ColoracionGrafos = () => {
   const [nodes, setNodes] = useState([]);
@@ -15,7 +16,7 @@ const ColoracionGrafos = () => {
   let color;
 
   const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(400, 400).parent(canvasParentRef);
+    p5.createCanvas(700, 400).parent(canvasParentRef);
   };
 
   const draw = (p5) => {
@@ -44,7 +45,12 @@ const ColoracionGrafos = () => {
   };
 
   const mouseClicked = (p5) => {
-    if (p5.mouseX > 0 && p5.mouseX < p5.width && p5.mouseY > 0 && p5.mouseY < p5.height) {
+    if (
+      p5.mouseX > 0 &&
+      p5.mouseX < p5.width &&
+      p5.mouseY > 0 &&
+      p5.mouseY < p5.height
+    ) {
       const selected = nodes.findIndex((node) => {
         const distance = p5.dist(node.x, node.y, p5.mouseX, p5.mouseY);
         return distance < 10;
@@ -63,23 +69,23 @@ const ColoracionGrafos = () => {
     const iterations = 10000;
     const startTimes = [];
     const endTimes = [];
-  
+
     for (let i = 0; i < iterations; i++) {
       startTimes.push(performance.now());
-  
+
       const graph = createAdjacencyMatrix(nodes, selectedNodes);
       color = new Array(V).fill(0);
       setUncoloredNodes((prevUncoloredNodes) => []); // Limpiar nodos sin colorear
-  
+
       for (let i = 0; i < V; i++) {
         let availableColors = new Array(colorCount).fill(true);
-  
+
         for (let j = 0; j < V; j++) {
           if (graph[i][j] && color[j]) {
             availableColors[color[j] - 1] = false;
           }
         }
-  
+
         let assigned = false;
         for (let c = 0; c < colorCount; c++) {
           if (availableColors[c]) {
@@ -88,31 +94,31 @@ const ColoracionGrafos = () => {
             break;
           }
         }
-  
+
         if (!assigned) {
           setColoringSuccessful(false);
           setUncoloredNodes((prevUncoloredNodes) => [...prevUncoloredNodes, i]);
         }
       }
-  
+
       const defaultColors = [
         { r: 255, g: 0, b: 0 }, // Rojo
         { r: 0, g: 0, b: 255 }, // Azul
         { r: 255, g: 255, b: 0 }, // Amarillo
-        { r: 0, g: 255, b: 0 },   // Verde
+        { r: 0, g: 255, b: 0 }, // Verde
       ];
-  
+
       const defaultColor = { r: 255, g: 255, b: 255 }; // Color predeterminado blanco
-  
+
       setColors(color.map((c) => (c ? defaultColors[c - 1] : defaultColor)));
-  
+
       endTimes.push(performance.now());
     }
-  
+
     const totalTime = endTimes[iterations - 1] - startTimes[0];
     const averageTime = totalTime / iterations;
     setExecutionTime(averageTime);
-  };  
+  };
 
   const createAdjacencyMatrix = (nodes, selectedNodes) => {
     const graph = Array.from({ length: V }, () => Array(V).fill(0));
@@ -139,25 +145,55 @@ const ColoracionGrafos = () => {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "row",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h4>Coloración de grafos</h4>
       <label>
         Cantidad de colores:
-        <input type="number" value={colorCount} onChange={(event) => setColorCount(Number(event.target.value))} />
+        <input
+          type="number"
+          value={colorCount}
+          onChange={(event) => setColorCount(Number(event.target.value))}
+        />
       </label>
-      <button onClick={handleLimpiarClick} style={{ marginTop: '20px' }}>Limpiar</button>
-      <Sketch setup={setup} draw={draw} mouseClicked={mouseClicked} />
-      <button onClick={handleColorearClick} style={{ marginTop: '20px' }}>Colorear</button>
+      <div
+        style={{ marginTop: "20px"}}
+      >
+        <Sketch setup={setup} draw={draw} mouseClicked={mouseClicked} />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button
+          variant="outline-success"
+          onClick={handleLimpiarClick}
+          style={{ marginRight: "10px" }}
+        >
+          Limpiar
+        </Button>
+        <Button
+          variant="outline-success"
+          onClick={handleColorearClick}
+          style={{ marginLeft: "10px" }}
+        >
+          Colorear
+        </Button>
+      </div>
       {executionTime !== null && (
-        <p style={{ marginTop: '10px' }}>
+        <p style={{ marginTop: "10px" }}>
           Tiempo de ejecución promedio: {executionTime.toFixed(4)} milisegundos
         </p>
       )}
-    {!coloringSuccessful && uncoloredNodes.length > 0 && (
-  <p style={{ color: 'red', marginTop: '10px' }}>
-    No se pudo colorear el grafo. {uncoloredNodes.length} nodo(s) no pudo(ieron) ser coloreado(s).
-  </p>
-)}
-
+      {!coloringSuccessful && uncoloredNodes.length > 0 && (
+        <p style={{ color: "red", marginTop: "10px" }}>
+          No se pudo colorear el grafo. {uncoloredNodes.length} nodo(s) no
+          pudo(ieron) ser coloreado(s).
+        </p>
+      )}
     </div>
   );
 };
